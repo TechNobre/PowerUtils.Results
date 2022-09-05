59ca8baeebf36d428e6136a50ae95820f9ef6ea7
Nelson Nobre <technobre@gmail.com>
2022-09-05 23:38:21 +0100

    feat: Override method `Result.GetType()`

---
+++ src/Types.cs
@@ -0,0 +1,10 @@
+﻿namespace PowerUtils.Results
+{
+    public interface IType { }
+
+#if NET6_0_OR_GREATER
+    public readonly record struct Success : IType;
+#else
+    public record Success : IType;
+#endif
+}
