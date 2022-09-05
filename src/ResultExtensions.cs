5c432855e5d1db97e7c24e0d481bf6fd9769861f
Nelson Nobre <technobre@gmail.com>
2022-09-05 19:31:37 +0100

    feat: Added extensions `FirstOrDefaultError`, `LastOrDefaultError`, `SingleOrDefaultError`

--- src/ResultExtensions.cs
+++ src/ResultExtensions.cs
@@ -11,15 +11,54 @@ namespace PowerUtils.Results
             => result.Errors.First();
 
         /// <summary>
+        /// Gets the first error, if does not contain errors return null
+        /// </summary>
+        public static IError FirstOrDefaultError(this IResult result)
+        {
+            if(!result.IsError)
+            {
+                return null;
+            }
+
+            return result.Errors.FirstOrDefault();
+        }
+
+        /// <summary>
         /// Gets the last error
         /// </summary>
         public static IError LastError(this IResult result)
             => result.Errors.Last();
 
         /// <summary>
+        /// Gets the last error, if does not contain errors return null
+        /// </summary>
+        public static IError LastOrDefaultError(this IResult result)
+        {
+            if(!result.IsError)
+            {
+                return null;
+            }
+
+            return result.Errors.LastOrDefault();
+        }
+
+        /// <summary>
         /// Gets the error when only exists one
         /// </summary>
         public static IError SingleError(this IResult result)
             => result.Errors.Single();
+
+        /// <summary>
+        /// Gets the error when only exists one, if does not contain errors return null
+        /// </summary>
+        public static IError SingleOrDefaultError(this IResult result)
+        {
+            if(!result.IsError)
+            {
+                return null;
+            }
+
+            return result.Errors.SingleOrDefault();
+        }
     }
 }
