b6bc3dc9bcecc89c646b9df9d3e49c97b68339b3
Nelson Nobre <technobre@gmail.com>
2022-09-06 13:21:10 +0100

    feat: Added extension `Result.ContainsError()`

--- src/ResultExtensions.cs
+++ src/ResultExtensions.cs
@@ -29,5 +29,49 @@ namespace PowerUtils.Results
 
             return predicate(result.Value);
         }
+
+
+
+        /// <summary>
+        /// Check if the result contains an error
+        /// </summary>
+        public static bool ContainsError(this IResult result) => result.IsError;
+
+        /// <summary>
+        /// Check if the result contains an error from a specific type
+        /// </summary>
+        public static bool ContainsError<TError>(this IResult result)
+            where TError : IError
+        {
+            if(!result.IsError)
+            {
+                return false;
+            }
+
+            return result.Errors.Any(a => a.GetType() == typeof(TError));
+        }
+
+        /// <summary>
+        /// Check if the result contains an error from a specific type and with a specific condition
+        /// </summary>
+        public static bool ContainsError<TError>(this IResult result, Func<IError, bool> predicate)
+            where TError : IError
+        {
+            if(!result.IsError)
+            {
+                return false;
+            }
+
+
+            foreach(var error in result.Errors)
+            {
+                if(error.GetType() == typeof(TError) && predicate(error))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
--- tests/PowerUtils.Results.Tests/ResultExtensionsTests.cs
+++ tests/PowerUtils.Results.Tests/ResultExtensionsTests.cs
@@ -168,5 +168,153 @@ namespace PowerUtils.Results.Tests
             // Assert
             act.Should().BeFalse();
         }
+
+
+
+        [Fact]
+        public void ResultWithoutErrors_ContainsError_False()
+        {
+            // Arrange
+            var result = Result.Ok();
+
+
+            // Act
+            var act = result.ContainsError();
+
+
+            // Assert
+            act.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ResultWithErrors_ContainsError_True()
+        {
+            // Arrange
+            Result result = _firstError;
+
+
+            // Act
+            var act = result.ContainsError();
+
+
+            // Assert
+            act.Should().BeTrue();
+        }
+
+
+
+
+
+
+        [Fact]
+        public void ResultWithoutErrors_ContainsSpecificErrorType_False()
+        {
+            // Arrange
+            var result = Result.Ok();
+
+
+            // Act
+            var act = result.ContainsError<NotFoundError>();
+
+
+            // Assert
+            act.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ResultWithGenericError_ContainsSpecificErrorType_False()
+        {
+            // Arrange
+            Result result = _firstError;
+
+
+            // Act
+            var act = result.ContainsError<NotFoundError>();
+
+
+            // Assert
+            act.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ResultWithForbiddenError_ContainsSpecificErrorType_False()
+        {
+            // Arrange
+            Result result = Error.Forbidden("fake", "fake", "fake");
+
+
+            // Act
+            var act = result.ContainsError<ForbiddenError>();
+
+
+            // Assert
+            act.Should().BeTrue();
+        }
+
+
+
+        [Fact]
+        public void ResultWithoutErrors_ContainsErrorWithCondition_False()
+        {
+            // Arrange
+            var result = Result.Ok();
+            static bool fakePredicate(IError x) => x.Code == "code";
+
+
+            // Act
+            var act = result.ContainsError<NotFoundError>(fakePredicate);
+
+
+            // Assert
+            act.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ResultWithGenericError_ContainsErrorWithConditionFromDifferentType_False()
+        {
+            // Arrange
+            Result result = _firstError;
+            static bool fakePredicate(IError x) => x.Code == "code";
+
+
+            // Act
+            var act = result.ContainsError<NotFoundError>(fakePredicate);
+
+
+            // Assert
+            act.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ResultWithForbiddenError_ContainsErrorWithDifferentConditionFromSameType_False()
+        {
+            // Arrange
+            Result result = Error.Forbidden("fake", "fake", "fake");
+            static bool fakePredicate(IError x) => x.Code == "code";
+
+
+            // Act
+            var act = result.ContainsError<ForbiddenError>(fakePredicate);
+
+
+            // Assert
+            act.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ResultWithForbiddenError_ContainsErrorWithSameConditionFromSameType_True()
+        {
+            // Arrange
+            Result result = Error.Conflict("fake", "code", "fake");
+            static bool fakePredicate(IError x) => x.Code == "code";
+
+
+            // Act
+            var act = result.ContainsError<ConflictError>(fakePredicate);
+
+
+            // Assert
+            act.Should().BeTrue();
+        }
     }
 }
