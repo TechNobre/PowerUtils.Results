b89fb9c32608262bd5cd70d5027afaa17d613cbb
Nelson Nobre <technobre@gmail.com>
2022-09-08 23:04:10 +0100

    chore: Added sample using MediatR

---
+++ samples/PowerUtils.Results.MediatR.Samples/Services/AddProductServiceHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using PowerUtils.Results.MediatR.Samples.DTOs;
+
+namespace PowerUtils.Results.MediatR.Samples.Services;
+
+public class AddProductServiceHandler : IRequestHandler<AddProductCommand, Result<ProductResponse>>
+{
+    public async Task<Result<ProductResponse>> Handle(AddProductCommand command, CancellationToken cancellationToken)
+    {
+        if(Random.Shared.Next(2) % 2 == 0)
+        {
+            return Error.Conflict(
+                nameof(command.Name),
+                "DUPLICATED",
+                $"Already exists a product with name '{command.Name}'"
+            );
+        }
+
+        return new ProductResponse
+        {
+            Id = Guid.NewGuid(),
+            Name = command.Name,
+            Quantity = command.Quantity,
+        };
+    }
+}
---
+++ samples/PowerUtils.Results.MediatR.Samples/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PowerUtils.Results.MediatR.Samples.Behaviors;
+
+namespace PowerUtils.Results.MediatR.Samples;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSwaggerGen();
+
+        services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints
+            => endpoints.MapControllers() // Mapping all controller
+        );
+    }
+}
