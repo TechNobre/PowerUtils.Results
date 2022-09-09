0723f97815a9d4f5bf97a782494b92b24c0b87e8
Nelson Nobre <technobre@gmail.com>
2022-09-09 11:13:14 +0100

    style: Organized `*.cs` files to easily distinguish what `ValueResult` and `VoidResult`

---
+++ src/IResult.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace PowerUtils.Results
+{
+    public interface IResult
+    {
+        /// <summary>
+        /// Gets a value indicating whether the state is error
+        /// </summary>
+        bool IsError { get; }
+
+        /// <summary>
+        /// Gets the list of errors
+        /// </summary>
+        IEnumerable<IError> Errors { get; }
+
+        void AddError(IError error);
+    }
+}
