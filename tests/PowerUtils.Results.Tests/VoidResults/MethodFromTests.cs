10140de94b9ae900422f57b49b060e195de2311c
Nelson Nobre <technobre@gmail.com>
2023-03-04 04:41:59 +0000

    feat: Added new factory `Result.From(List<IError> errors)`

--- tests/PowerUtils.Results.Tests/VoidResults/MethodFromTests.cs
+++ tests/PowerUtils.Results.Tests/VoidResults/MethodFromTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using FluentAssertions.Execution;
 using PowerUtils.Results.Tests.Fakes;
 using Xunit;
 
@@ -7,7 +9,7 @@ namespace PowerUtils.Results.Tests.VoidResults
     public class MethodFromTests
     {
         [Fact]
-        public void CustomError_ResultFrom_Result()
+        public void CustomError_ResultFrom_ResultWithError()
         {
             // Arrange
             var property = "fakeCustomProperty";
@@ -26,6 +28,44 @@ namespace PowerUtils.Results.Tests.VoidResults
                 property,
                 code,
                 description);
+        }
+
+        [Fact]
+        public void ErrorList_ResultFrom_ResultWithError()
+        {
+            // Arrange
+            var property1 = "fakeProperty1";
+            var code1 = "fakeCode1";
+            var description1 = "fakeDescription1";
+
+            var property2 = "fakeProperty2";
+            var code2 = "fakeCode2";
+            var description2 = "fakeDescription2";
+
+            var errors = new List<IError>
+            {
+                new CustomError(property1, code1, description1),
+                new ForbiddenError(property2, code2, description2)
+            };
+
+
+            // Act
+            var act = Result.From(errors);
+
+
+            // Assert
+            using(new AssertionScope())
+            {
+                act.Should().ContainsError<CustomError>(
+                    property1,
+                    code1,
+                    description1);
+
+                act.Should().ContainsError<ForbiddenError>(
+                    property2,
+                    code2,
+                    description2);
+            }
         }
     }
 }
