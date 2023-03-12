5b49c94692c50c75575ccba4302c5c7d3ea0328b
Nelson Nobre <technobre@gmail.com>
2023-03-12 01:03:30 +0000

    refactor: resolvido code smells

---
+++ src/Serializers/SerializerConstants.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace PowerUtils.Results.Serializers
+{
+    internal class SerializerConstants
+    {
+        internal const string TYPE_NAME = "_type";
+
+        internal static readonly JsonEncodedText PROPERTY_NAME = JsonEncodedText.Encode(nameof(IError.Property));
+        internal static readonly JsonEncodedText CODE_NAME = JsonEncodedText.Encode(nameof(IError.Code));
+        internal static readonly JsonEncodedText DESCRIPTION_NAME = JsonEncodedText.Encode(nameof(IError.Description));
+    }
+}
